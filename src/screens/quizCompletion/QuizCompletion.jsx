5e74a70b0683dc224d1ce09c91258494a2c230a3
priyanshu <priyanshuqpwp@gmail.com>
2023-10-21 23:24:25 +0530

    completed quiz completed route

--- src/screens/quizCompletion/QuizCompletion.jsx
+++ src/screens/quizCompletion/QuizCompletion.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import styles from "./QuizCompletion.module.css"
+import TrophyImage from "../../assets/images/trophy-image.png"
 
 const QuizCompletion = () => {
   return (
-    <div className={styles.mainContainer}>QuizCompletion</div>
+    <div className={styles.mainContainer}>
+        <div className={styles.thankYouContainer}>
+          <div className={styles.headding}>Congrats Quiz is completed</div>
+          <img src={TrophyImage} alt="" className={styles.trophyImage}/>
+          <div className={styles.quizScore}>Your Score is <span className={styles.scoreColor}> 03/04</span></div>
+        </div>
+      </div>
   )
 }
 
