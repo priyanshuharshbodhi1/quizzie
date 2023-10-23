73568ff93be84b9d61484348bfb20042746f059f
priyanshu <priyanshuqpwp@gmail.com>
2023-10-23 23:50:25 +0530

    made question route

---
+++ src/screens/questions/Questions.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./Questions.module.css"
+
+const question = () => {
+  return (
+    <div className={styles.mainContainer}>question</div>
+  )
+}
+
+export default question
