a273a0d0684ec77e5fd83370b6a2a922bd75443b
priyanshu <priyanshuqpwp@gmail.com>
2023-10-26 11:39:56 +0530

    mobile responsive DONE

--- src/screens/questions/Questions.jsx
+++ src/screens/questions/Questions.jsx
@@ -3,7 +3,24 @@ import styles from "./Questions.module.css"
 
 const question = () => {
   return (
-    <div className={styles.mainContainer}>question</div>
+    <>
+    <div className={styles.mainContainer}>
+        <div className={styles.questionContent}>
+            <div className={styles.header}>
+                <div className={styles.questionNumber}>01/04</div>
+                <div className={styles.timer}>00:00s</div>
+            </div>
+            <div className={styles.pollQuestion}>What is your name and where are you from?</div>
+            <div className={styles.options}>
+                <div className={styles.option}>Option1 <img src="" alt="" /></div>
+                <div className={styles.option}>option2 <img src="" alt="" /></div>
+                <div className={styles.option}>option3 <img src="" alt="" /></div>
+                <div className={styles.option}>option4 <img src="" alt="" /></div>
+            </div>
+            <button className={styles.nextBtn}>NEXT</button>
+        </div>
+    </div>
+    </>
   )
 }
 
